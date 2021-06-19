7604b629794d8b41f9b3bd65f6458c9265722d28
ParkJeongwoong <77447841+ParkJeongwoong@users.noreply.github.com>
2021-06-20 02:54:55 +0900

    update

--- React/react_hook/src/App.js
+++ React/react_hook/src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 
 // useInput
 const useInput = (initialValue, validator) => {
@@ -49,6 +49,72 @@ const useTitle = (initialTitle) => {
   return setTitle
 }
 
+// useClick
+const useClick = (onClick) => {
+  const element = useRef()
+  useEffect(() => {
+    // 1. 변수를 안에서 copy하지 않으면 warning (element가 unmount 시점에 null이 된다.)
+    const ref = element.current
+    if(ref){
+      ref.addEventListener("click", onClick)
+    }
+    if(typeof onClick !== "function") {
+      return
+    }
+    // 2. Unmount 단계, Component가 Unmount되면 eventListener를 정리해주는 게 맞다
+    return () => {
+      if (ref) {
+        ref.removeEventListener("click", onClick)
+      }
+    }
+  }, [onClick])
+  if(typeof onClick !== "function") {
+    return
+  }
+  return element
+}
+
+// useConfirm & usePreventLeave
+const useConfirm = (message = "", onConfirm, onCancel) => {
+  if (typeof onConfirm !== "function") {
+    return
+  }
+  if (typeof onCancel !== "function") {
+    return
+  }
+  const confirmAction = () => {
+    if(window.confirm(message)) {
+      onConfirm()
+    } else {
+      onCancel()
+    }
+  }
+  return confirmAction
+}
+
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault()
+    event.returnValue = ""
+  }
+  const enablePrevent = () => window.addEventListener("beforeunload", listener)
+  const disablePrevent = () => window.removeEventListener("beforeunload", listener)
+  return { enablePrevent, disablePrevent }
+}
+
+// useBeforeLeave
+const useBeforeLeave = (onBefore) => {
+  const handle = event => {
+    const { clientY } = event
+    if(clientY <= 0) {
+      onBefore()
+    }
+  }
+  useEffect(() => {
+    document.addEventListener("mouseleave", handle)
+    return () => document.removeEventListener("mouseleave", handle)
+  }, [])
+}
 
 
 const App = () => {
@@ -74,6 +140,22 @@ const App = () => {
   const titleUpdater = useTitle("Loading...")
   setTimeout(()=>titleUpdater("Home"), 5000)
 
+  // useClick
+  const sayClicked = () => console.log("I'm clicked!")
+  const title = useClick(sayClicked)
+
+  // useConfirm & usePreventLeave
+  const deleteWorld = () => console.log("Deleting the world...")
+  const abort = () => console.log("Aborted")
+  const confirmDelete = useConfirm("Are you sure", deleteWorld, abort)
+
+  const {enablePrevent, disablePrevent} = usePreventLeave()
+
+  // useBeforeLeave
+  const begForLife = () => console.log("Pls dont Leave")
+  useBeforeLeave(begForLife)
+
+    
   return (
     <div className="App">
       {/* useState */}
@@ -104,6 +186,19 @@ const App = () => {
       <div>Hi</div>
       <button onClick={()=>setNumber(number+1)}>{ number }</button>
       <button onClick={()=>setAnumber(aNumber+1)}>{ aNumber }</button>
+
+      {/* useClick */}
+      <h1 ref={title}>USECLICK</h1>
+
+      {/* useConfirm & usePreventLeave */}
+      <button onClick={confirmDelete}>Delete the World</button>
+      <div>
+        <button onClick={enablePrevent}>Protect</button>
+        <button onClick={disablePrevent}>Unprotect</button>
+      </div>
+
+      {/* useBeforeLeave */}
+      <h1>BeforeYouLeave</h1>
     </div>
   )
 }
